162e082decbb86ff823b21ff9134d948112824ea
Richard Chalk <barmybrit@gmail.com>
2023-02-24 06:37:14 +0100

    Add project files.

---
+++ SuperHeroAPIDemo_VG_Start/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHeroAPIDemo_G.Models;
+using System.Collections.Generic;
+
+namespace SuperHeroAPIDemo_G.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+
+}
